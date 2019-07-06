011be2dc816a683961b97746bb7d9257a57ed560
cacadosman <wetmanz23@gmail.com>
2019-07-06 11:53:26 +0700

    Edit auth controller

--- src/main/java/com/future/onlinetraining/users/controller/AuthController.java
+++ src/main/java/com/future/onlinetraining/users/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,4 +36,16 @@ public class AuthController {
                 .send();
     }
 
+    @GetMapping("/auth/_role")
+    public ResponseEntity getRole(){
+        if(userService.getUserFromSession() == null){
+            return userService.unauthenticated();
+        }
+
+        return new ResponseHelper<>()
+                .setSuccessStatus(true)
+                .setHttpStatus(HttpStatus.OK)
+                .setParam("role", userService.getUserFromSession().getRole().getValue())
+                .send();
+    }
 }
