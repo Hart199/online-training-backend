2e342f0b5a850cb40965a3dd0d67fd8ae65338b6
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:11:09 +0700

    add module request like repository

---
+++ src/main/java/com/future/onlinetraining/repository/ModuleRequestLikeRepository.java
@@ -0,0 +1,10 @@
+package com.future.onlinetraining.repository;
+
+import com.future.onlinetraining.entity.ModuleRequestLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleRequestLikeRepository extends JpaRepository<ModuleRequestLike, Integer> {
+
+
+
+}
