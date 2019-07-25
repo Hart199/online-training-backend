906a741badafb3036dacf88f01d9930006ba60b7
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:26:01 +0700

    edit trainer rating repo

--- src/main/java/com/future/onlinetraining/repository/TrainerRatingRepository.java
+++ src/main/java/com/future/onlinetraining/repository/TrainerRatingRepository.java
@@ -1,7 +1,9 @@
 package com.future.onlinetraining.repository;
 
 import com.future.onlinetraining.entity.TrainerRating;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainerRatingRepository extends JpaRepository<TrainerRating, Integer> {
+
 }
