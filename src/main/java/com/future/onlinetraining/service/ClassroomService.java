b5e4fe90f6f58930ba1493b64e3d1a95144d567c
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:28:55 +0700

    edit some methods on classroom service

--- src/main/java/com/future/onlinetraining/service/ClassroomService.java
+++ src/main/java/com/future/onlinetraining/service/ClassroomService.java
@@ -1,12 +1,14 @@
 package com.future.onlinetraining.service;
 
 import com.future.onlinetraining.entity.Classroom;
+import com.future.onlinetraining.entity.projection.ClassroomSubscribed;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ClassroomService {
 
-    public Page<Classroom> getAllPageableClassroom();
+    Page<Classroom> getAllPageableClassroom();
     Page<Classroom> getAll(Pageable pageable);
+    Page<ClassroomSubscribed> getAllSubscribed(int page, int size);
 
 }
