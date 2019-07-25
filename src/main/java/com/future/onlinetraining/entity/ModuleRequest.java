9ebc99472940f20d0a6298a2a2da189c045f0a0e
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:27:54 +0700

    edit module request entity to add some properties

--- src/main/java/com/future/onlinetraining/entity/ModuleRequest.java
+++ src/main/java/com/future/onlinetraining/entity/ModuleRequest.java
@@ -1,11 +1,16 @@
 package com.future.onlinetraining.entity;
 
+import com.future.onlinetraining.repository.ModuleRequestLikeRepository;
+import com.future.onlinetraining.users.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Builder
@@ -21,8 +26,15 @@ public class ModuleRequest {
 
     private String title;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     private ModuleCategory moduleCategory;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @OneToMany(mappedBy = "moduleRequest")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<ModuleRequestLike> moduleRequestLikes;
 
     private String status;
 
