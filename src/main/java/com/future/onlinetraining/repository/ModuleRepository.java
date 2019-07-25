6ea5214126c467062c5221187c01497e1dd45fd8
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:10:32 +0700

    add search module query on module repository

--- src/main/java/com/future/onlinetraining/repository/ModuleRepository.java
+++ src/main/java/com/future/onlinetraining/repository/ModuleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
 
@@ -30,5 +31,27 @@ public interface ModuleRepository extends JpaRepository<Module, Integer> {
                     "group by m.id, mc.id "
     )
     Page<ModuleData> getAllModule(Pageable pageable);
+
+    @Query (
+            nativeQuery = true,
+            value = "select m.id, m.name as name, avg(mr.value) as rating, m.description as desc, " +
+                    "m.time_per_session as timePerSession, mc.name as category, " +
+                    "count(c.id) as classroomCount, count(ms.id) as sessionCount " +
+                    "from modules m " +
+                    "inner join module_ratings mr " +
+                    "on m.id = mr.module_id " +
+                    "inner join classrooms c " +
+                    "on m.id = c.module_id " +
+                    "inner join module_sessions ms " +
+                    "on m.id = ms.module_id " +
+                    "inner join module_categories mc " +
+                    "on mc.id = m.module_category_id " +
+                    "where (:nameParam is null or lower(m.name) like concat('%', :nameParam, '%')) " +
+                    "and (:categoryParam is null or mc.name = concat(:categoryParam)) " +
+                    "group by m.id, mc.id "
+    )
+    Page<ModuleData> getAllBySearhTerm(
+            Pageable pageable, @Param("nameParam") String name,
+            @Param("categoryParam") String category);
 
 }
