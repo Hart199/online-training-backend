064c5ac2119b326fb3e3ce4e57c49f36207450cd
cacadosman <wetmanz23@gmail.com>
2019-07-25 21:25:44 +0700

    add find all by name param query on module request repo

--- src/main/java/com/future/onlinetraining/repository/ModuleRequestRepository.java
+++ src/main/java/com/future/onlinetraining/repository/ModuleRequestRepository.java
@@ -1,7 +1,19 @@
 package com.future.onlinetraining.repository;
 
 import com.future.onlinetraining.entity.ModuleRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ModuleRequestRepository extends JpaRepository<ModuleRequest, Integer> {
+
+    Page<ModuleRequest> findAll(Pageable pageable);
+
+    @Query(
+            value = "select mr from ModuleRequest mr " +
+                    "where (:nameParam is null or lower(mr.title) like %:nameParam%) "
+    )
+    Page<ModuleRequest> findAllByNameParam(Pageable pageable, @Param("nameParam") String name);
 }
