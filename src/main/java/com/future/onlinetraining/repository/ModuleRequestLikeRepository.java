aa3bea3175260db353b96af27d9f3a086c76826d
cacadosman <wetmanz23@gmail.com>
2019-07-26 14:26:22 +0700

    add query to find by module id and user id on module request like repo

--- src/main/java/com/future/onlinetraining/repository/ModuleRequestLikeRepository.java
+++ src/main/java/com/future/onlinetraining/repository/ModuleRequestLikeRepository.java
@@ -2,9 +2,13 @@ package com.future.onlinetraining.repository;
 
 import com.future.onlinetraining.entity.ModuleRequestLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ModuleRequestLikeRepository extends JpaRepository<ModuleRequestLike, Integer> {
 
-
-
+    @Query(
+            value = "from ModuleRequestLike mrl where mrl.moduleRequest.id = :id and mrl.user.id = :userId"
+    )
+    ModuleRequestLike findByIdAndUserId(@Param("id") int id, @Param("userId") int userId);
 }
