60000229571d763b62051726ea732ceb9e163d5b
cacadosman <wetmanz23@gmail.com>
2019-07-26 14:25:47 +0700

    add implementation to store module request and vote like on module request service

--- src/main/java/com/future/onlinetraining/service/ModuleRequestService.java
+++ src/main/java/com/future/onlinetraining/service/ModuleRequestService.java
@@ -1,9 +1,14 @@
 package com.future.onlinetraining.service;
 
+import com.future.onlinetraining.dto.ModuleRequestDTO;
+import com.future.onlinetraining.dto.ModuleRequestLikeDTO;
 import com.future.onlinetraining.entity.ModuleRequest;
+import com.future.onlinetraining.entity.ModuleRequestLike;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ModuleRequestService {
     Page<ModuleRequest> getAll(Pageable pageable, String name);
+    ModuleRequest store(ModuleRequestDTO moduleRequestDTO);
+    <T> T voteLike(ModuleRequestLikeDTO moduleRequestLikeDTO);
 }
--- src/main/java/com/future/onlinetraining/service/impl/ModuleRequestServiceImpl.java
+++ src/main/java/com/future/onlinetraining/service/impl/ModuleRequestServiceImpl.java
@@ -1,8 +1,15 @@
 package com.future.onlinetraining.service.impl;
 
+import com.future.onlinetraining.dto.ModuleRequestDTO;
+import com.future.onlinetraining.dto.ModuleRequestLikeDTO;
+import com.future.onlinetraining.entity.ModuleCategory;
 import com.future.onlinetraining.entity.ModuleRequest;
+import com.future.onlinetraining.entity.ModuleRequestLike;
+import com.future.onlinetraining.repository.ModuleCategoryRepository;
+import com.future.onlinetraining.repository.ModuleRequestLikeRepository;
 import com.future.onlinetraining.repository.ModuleRequestRepository;
 import com.future.onlinetraining.service.ModuleRequestService;
+import com.future.onlinetraining.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +20,48 @@ public class ModuleRequestServiceImpl implements ModuleRequestService {
 
     @Autowired
     ModuleRequestRepository moduleRequestRepository;
+    @Autowired
+    ModuleCategoryRepository moduleCategoryRepository;
+    @Autowired
+    ModuleRequestLikeRepository moduleRequestLikeRepository;
+    @Autowired
+    UserService userService;
 
     public Page<ModuleRequest> getAll(Pageable pageable, String name) {
         return moduleRequestRepository.findAllByNameParam(pageable, name);
     }
+
+    public ModuleRequest store(ModuleRequestDTO moduleRequestDTO) {
+        ModuleCategory moduleCategory = moduleCategoryRepository.findByName(moduleRequestDTO.getCategory());
+        if (moduleCategory == null)
+            return null;
+
+        ModuleRequest moduleRequest = ModuleRequest.builder()
+                .title(moduleRequestDTO.getTitle())
+                .user(userService.getUserFromSession())
+                .status("waiting")
+                .moduleCategory(moduleCategory)
+                .build();
+
+        return moduleRequestRepository.save(moduleRequest);
+    }
+
+    public <T> T voteLike(ModuleRequestLikeDTO moduleRequestLikeDTO) {
+        ModuleRequestLike moduleRequestLike = moduleRequestLikeRepository.findByIdAndUserId(
+                moduleRequestLikeDTO.getModuleId(), userService.getUserFromSession().getId());
+
+        if (moduleRequestLike != null) {
+            moduleRequestLikeRepository.delete(moduleRequestLike);
+            return null;
+        }
+
+        ModuleRequest moduleRequest = moduleRequestRepository.getOne(moduleRequestLikeDTO.getModuleId());
+        return (T) moduleRequestLikeRepository.save(
+                ModuleRequestLike.builder()
+                .moduleRequest(moduleRequest)
+                .user(userService.getUserFromSession())
+                .build()
+        );
+    }
+
 }
