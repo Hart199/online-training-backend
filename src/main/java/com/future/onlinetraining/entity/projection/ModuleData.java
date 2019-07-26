5f72673e3171b85b1efb2965be29cbabda02df7e
cacadosman <wetmanz23@gmail.com>
2019-07-26 21:02:53 +0700

    add some props to module data projection

--- src/main/java/com/future/onlinetraining/entity/projection/ModuleData.java
+++ src/main/java/com/future/onlinetraining/entity/projection/ModuleData.java
@@ -9,6 +9,9 @@ public interface ModuleData {
     String getTimePerSession();
     String getCategory();
     int getClassroomCount();
+    int getOpenClassroomCount();
+    int getClosedClassroomCount();
     int getSessionCount();
+    boolean getHasExam();
 
 }
