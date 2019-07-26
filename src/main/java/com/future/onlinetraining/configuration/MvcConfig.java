42a98acb075c04b547dc9e146ce168f7d10eced5
cacadosman <wetmanz23@gmail.com>
2019-07-26 14:25:07 +0700

    just commenting some code

--- src/main/java/com/future/onlinetraining/configuration/MvcConfig.java
+++ src/main/java/com/future/onlinetraining/configuration/MvcConfig.java
@@ -3,7 +3,10 @@ package com.future.onlinetraining.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
@@ -13,5 +16,33 @@ public class MvcConfig {
     BCryptPasswordEncoder getBcryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+//            }
+//        };
+//    }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*");
+//    }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedHeaders("Origin", "Content-Type", "X-Auth-Token")
+//                        .allowedMethods("*");
+//            }
+//        };
+//    }
 
 }
--- src/main/java/com/future/onlinetraining/configuration/SecurityConfig.java
+++ src/main/java/com/future/onlinetraining/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.future.onlinetraining.configuration;
 
 import com.future.onlinetraining.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,11 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/css/**");
         web.ignoring().antMatchers("/profile/pict/**");
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+//                .cors().and()
                 .authorizeRequests()
                 .antMatchers("/login", "/register").permitAll()
                 .and()
@@ -63,5 +76,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             response.sendRedirect("/auth/failed");
         };
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:8081"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
 }
