4f08d9162297699b1a67b04f4de7a1944b254743
cacadosman <wetmanz23@gmail.com>
2019-07-26 14:27:22 +0700

    add mapping to store module request and vote like on module request controller

--- src/main/java/com/future/onlinetraining/controller/ModuleRequestController.java
+++ src/main/java/com/future/onlinetraining/controller/ModuleRequestController.java
@@ -1,14 +1,18 @@
 package com.future.onlinetraining.controller;
 
+import com.future.onlinetraining.dto.ModuleRequestDTO;
+import com.future.onlinetraining.dto.ModuleRequestLikeDTO;
+import com.future.onlinetraining.entity.ModuleRequest;
+import com.future.onlinetraining.entity.ModuleRequestLike;
 import com.future.onlinetraining.service.ModuleRequestService;
 import com.future.onlinetraining.utility.ResponseHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class ModuleRequestController {
@@ -26,4 +30,35 @@ public class ModuleRequestController {
                 .send();
     }
 
+    @PostMapping("/modules")
+    public ResponseEntity store(@RequestBody @Valid ModuleRequestDTO moduleRequestDTO) {
+        ResponseHelper responseHelper = new ResponseHelper();
+        ModuleRequest moduleRequest = moduleRequestService.store(moduleRequestDTO);
+
+        if (moduleRequest == null)
+            return responseHelper
+                    .setSuccessStatus(false)
+                    .setMessage("Failed.")
+                    .send();
+
+        return  responseHelper
+                    .setSuccessStatus(true)
+                    .setParam("data", moduleRequest)
+                    .setMessage("Success")
+                    .send();
+    }
+
+    @PostMapping("/modules/_likes")
+    public ResponseEntity voteLike(@RequestBody @Valid ModuleRequestLikeDTO moduleRequestLikeDTO) {
+        ModuleRequestLike moduleRequestLike = moduleRequestService.voteLike(moduleRequestLikeDTO);
+
+        if (moduleRequestLike == null)
+            return new ResponseHelper<>()
+                .setMessage("Unvote")
+                .send();
+
+        return new ResponseHelper<>()
+                .setMessage("Voted")
+                .send();
+    }
 }
