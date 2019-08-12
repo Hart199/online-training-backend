44f165ecc30eb5b4790b9c3fcdd9a620662e9204
cacadosman <wetmanz23@gmail.com>
2019-08-12 23:11:37 +0700

    refactor query

--- src/main/java/com/future/onlinetraining/repository/ClassroomRepository.java
+++ src/main/java/com/future/onlinetraining/repository/ClassroomRepository.java
@@ -26,7 +26,7 @@ public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
     )
     Page<Classroom> findSubscribed(
             Pageable pageable, @Param("userId") int userId, @Param("status") String status);
-    
+
     @Query(
             value = "select new com.future.onlinetraining.entity.projection.ClassroomData(" +
                     "c.id, c.name, m.name, t.fullname, c.status, c.min_member, c.max_member, " +
@@ -39,14 +39,9 @@ public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
                     "inner join c.trainer t " +
                     "left join c.classroomRequests cr " +
                     "left join c.classroomResults cres " +
-                    "left join c.classroomSessions cs " +
                     "left join m.moduleRatings mrg " +
                     "where (:nameParam is null or lower(m.name) like %:nameParam%) " +
-                    "and (:hasExam is null or " +
-                    "((select count(cs) from cs where cs.isExam = true) > 0 " +
-                    "and :hasExam = true) or " +
-                    "((select count(cs) from cs where cs.isExam = true) = 0 " +
-                    "and :hasExam = false)) " +
+                    "and (:hasExam is null or m.hasExam = :hasExam) " +
                     "group by c, m, t"
     )
     Page<ClassroomData> all(Pageable pageable, @Param("nameParam") String name, @Param("hasExam") Boolean hasExam);
