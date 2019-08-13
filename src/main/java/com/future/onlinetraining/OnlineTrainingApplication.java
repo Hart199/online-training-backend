50f279674d591df855f6bdaa6a384c80ca081d08
cacadosman <wetmanz23@gmail.com>
2019-08-13 09:22:10 +0700

    add @EnableCaching annontation

--- src/main/java/com/future/onlinetraining/OnlineTrainingApplication.java
+++ src/main/java/com/future/onlinetraining/OnlineTrainingApplication.java
@@ -2,8 +2,10 @@ package com.future.onlinetraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OnlineTrainingApplication {
 
     public static void main(String[] args) {
