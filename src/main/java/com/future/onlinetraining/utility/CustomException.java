53678dc012b166883b8e949a8c52dbfd96125578
cacadosman <wetmanz23@gmail.com>
2019-08-19 08:59:43 +0700

    remove unused class

--- src/main/java/com/future/onlinetraining/utility/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.future.onlinetraining.utility;
-
-public class CustomException extends RuntimeException{
-
-    public CustomException(String message) {
-        super(message);
-    }
-
-}
